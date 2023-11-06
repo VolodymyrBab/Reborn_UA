f9588f45811e88c36c1766e390e23ac987598dc1
VolodymyrBab <volbv@yahoo.com>
2023-11-06 22:42:55 +0100

    3333

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import App from './components/App';
+import {App} from './components/App';
 import reportWebVitals from './components/reportWebVitals';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
