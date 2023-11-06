1fce4b0b284ded153b3ae5da400a01f9d8482299
VolodymyrBab <volbv@yahoo.com>
2023-11-06 22:34:44 +0100

    33

--- src/index.js
+++ src/index.js
@@ -6,10 +6,9 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import App from './components/App';
 import reportWebVitals from './components/reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>
 );
 
